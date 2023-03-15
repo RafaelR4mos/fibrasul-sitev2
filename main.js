f972114390c874ac31a50678bb93511ed417f683
Rafael <rafaelramos0401@gmail.com>
2023-03-15 20:28:23 -0300

    made objectives and after-before-pic update dinamically based on the object

--- main.js
+++ main.js
@@ -13,13 +13,16 @@ const mainCarouselBtn = document.querySelector(".carousel-btn");
 const cardsSection = document.querySelector("#cards-section");
 const cardsContainer = document.querySelector(".cards-container");
 
-const portfolioSection = document.querySelector("#portfolio-section");
 const partnersSection = document.querySelector("#partners-section");
 const dataSection = document.querySelector("#data-section");
 const teamsSection = document.querySelector("#team-section");
 
+const portfolioSection = document.querySelector("#portfolio-section");
 const portfolioBtn = document.querySelector(".portfolio-btn");
 const portfolioBulletNavParents = document.querySelector(".controls ul");
+const porfolioContainer = document.querySelector(".slider");
+const portfolioServices = document.querySelector(".slider section");
+
 const mainCarouselBulletNavParents = document.querySelector(
     ".main-carousel-controls ul"
 );
@@ -63,6 +66,105 @@ const cardInfo = [
     },
 ];
 
+const portfolioInfo = [
+    {
+        id: 1,
+        projectTitle: "Teste 1",
+        projectPeriod: "20/12/2023 à 21/12/2023",
+        description:
+            "1-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur.",
+        beforeImgSrc: "./assets/gear-icon.svg",
+        afterImgSrc: "./assets/medal-icon.svg",
+        objectives: {
+            objective1:
+                "Uma descricao Lorem Ipsum has been the industry s standard dummy text ever since the 1500s.",
+            objective2:
+                "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+            objective3:
+                "2aaaaaaaLorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+        },
+        hirers: {
+            hirer1: "./assets/images/logos/cmpc.png",
+        },
+        partners: {
+            partner1: "./assets/images/logos/eletromont.png",
+            partner2: "./assets/images/logos/estrutural.png",
+        },
+    },
+    {
+        id: 2,
+        projectTitle: "Teste 2",
+        projectPeriod: "20/12/2023 à 21/12/2023",
+        description:
+            "2-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur.",
+        beforeImgSrc: "./assets/gear-icon.svg",
+        afterImgSrc: "./assets/medal-icon.svg",
+        objectives: {
+            objective1:
+                "Lorem Ipsum has been the industry s standard dummy text ever since the 1500s.2",
+            objective2:
+                "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+            objective3:
+                "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+        },
+        hirers: {
+            hirer1: "./assets/images/logos/cmpc.png",
+        },
+        partners: {
+            partner1: "./assets/images/logos/eletromont.png",
+            partner2: "./assets/images/logos/estrutural.png",
+        },
+    },
+    {
+        id: 3,
+        projectTitle: "Teste 3",
+        projectPeriod: "20/12/2023 à 21/12/2023",
+        description:
+            "1-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur.",
+        beforeImgSrc: "./assets/gear-icon.svg",
+        afterImgSrc: "./assets/medal-icon.svg",
+        objectives: {
+            objective1:
+                "aaaaaaaaaaLorem Ipsum has been the industry s standard dummy text ever since the 1500s.",
+            objective2:
+                "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+            objective3:
+                "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+        },
+        hirers: {
+            hirer1: "./assets/images/logos/cmpc.png",
+        },
+        partners: {
+            partner1: "./assets/images/logos/eletromont.png",
+            partner2: "./assets/images/logos/estrutural.png",
+        },
+    },
+    {
+        id: 4,
+        projectTitle: "Teste 3",
+        projectPeriod: "20/12/2023 à 30/12/2023",
+        description:
+            "1-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, temporibus provident ab distinctio vitae nemo omnis dignissim os maiores dolores enim magni. Quos molestiae itaque corporis autem recusandae. Eius, laborum tenetur.",
+        beforeImgSrc: "./assets/gear-icon.svg",
+        afterImgSrc: "./assets/medal-icon.svg",
+        objectives: {
+            objective1:
+                "Lorem Ipsum has been the industry s standard dummy text ever since the 1500s.",
+            objective2:
+                "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+            objective3:
+                "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+        },
+        hirers: {
+            hirer1: "./assets/images/logos/cmpc.png",
+        },
+        partners: {
+            partner1: "./assets/images/logos/eletromont.png",
+            partner2: "./assets/images/logos/estrutural.png",
+        },
+    },
+];
+
 function createCards(i) {
     const cards = document.querySelector(".cards-container .card");
 
@@ -74,12 +176,58 @@ function createCards(i) {
     }
 }
 
+function createServices(i) {
+    if (i < cardInfo.length - 1) {
+        let portfolioClone = portfolioServices.cloneNode(true);
+        porfolioContainer.appendChild(portfolioClone);
+    }
+}
+
 function insertCardContent() {
     document.querySelectorAll(".card").forEach((elem, i) => {
         const cardTitle = cardInfo[i].title;
         elem.querySelector(".card-title").innerText = cardTitle;
         const cardContent = cardInfo[i].content;
         elem.querySelector("p").innerText = cardContent;
+    });
+}
+
+function insertPortfolioServicesContent() {
+    document.querySelectorAll(".slider section").forEach((service, i) => {
+        const serviceTitle = portfolioInfo[i].projectTitle;
+        const projectPeriod = portfolioInfo[i].projectPeriod;
+        const description = portfolioInfo[i].description;
+        const projectObjetives = [
+            portfolioInfo[i].objectives.objective1,
+            portfolioInfo[i].objectives.objective2,
+            portfolioInfo[i].objectives.objective3,
+        ];
+        const serviceImages = [
+            portfolioInfo[i].afterImgSrc,
+            portfolioInfo[i].beforeImgSrc,
+        ];
+        const hirers = [portfolioInfo[i].hirers.hirer1];
+        const partners = [
+            portfolioInfo[i].partners.partner1,
+            portfolioInfo[i].partners.partner2,
+        ];
+
+        service.querySelector(".service-header h2").innerText = serviceTitle;
+        service.querySelector(".service-header span").innerText = projectPeriod;
+        service.querySelector(".service-info-container p").innerText =
+            description;
+
+        service
+            .querySelectorAll(".scope-objectives ul li")
+            .forEach((item, i) => {
+                item.innerText = projectObjetives[i];
+            });
+
+        service
+            .querySelectorAll(".after-before-pic img")
+            .forEach((serviceImg, i) => {
+                serviceImg.setAttribute("src", serviceImages[i]);
+            });
     });
 }
 
@@ -101,7 +249,12 @@ document.addEventListener("DOMContentLoaded", () => {
     cardInfo.forEach((card, i) => {
         createCards(i);
     });
+
+    portfolioInfo.forEach((service, i) => {
+        createServices(i);
+    });
     insertCardContent();
+    insertPortfolioServicesContent();
 
     cardsSection.style.paddingTop = headerHeight + "px";
     portfolioSection.style.paddingTop = headerHeight + "px";
